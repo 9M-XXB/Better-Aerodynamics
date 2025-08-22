6d5ca0f0c98094b089be9f59288073002ff42f49
9M-XXB <hao7531@outlook.com>
2025-08-21 20:44:28 -0500

    initial commit

---
+++ src/client/java/rfb/betteraero/BetterAerodynamicsClient.java
@@ -0,0 +1,10 @@
+package rfb.betteraero;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterAerodynamicsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
